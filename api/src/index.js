2f25c5863d94f8c8a33cf957f3427c21632214e1
Chantal <chantiicou@gmail.com>
2025-04-29 08:52:14 -0300

    cambiando index - 4

--- api/src/index.js
+++ api/src/index.js
@@ -9,12 +9,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares globales
-app.use(cors({
-  origin: "https://webalchemist-2.onrender.com",
-  // origin: "http://localhost:3000", // Alternativa para desarrollo local
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "https://webalchemist-2.onrender.com",
+    // origin: "http://localhost:3000", // Alternativa para desarrollo local
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -25,23 +27,24 @@ app.use(bodyParser.raw({ type: "application/octet-stream" }));
 // Rutas de la API
 app.use("/", chatBotRoutes);
 
-
 // Sirve el frontend React (solo en producción)
 if (process.env.NODE_ENV === "production") {
   // Configura la ruta correcta para los archivos estáticos del frontend
-  app.use(express.static(path.join(__dirname, "../../client_temp/build")));
-  console.log(path.join(__dirname, "../client_temp/build"));
+  app.use(express.static(path.join(__dirname, "../../client/build")));
+  console.log(path.join(__dirname, "../client/build"));
 
   // Ruta catch-all para servir el frontend
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../client_temp/build", "index.html"));
+    res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
   });
 }
 
 // Middleware global de manejo de errores (después de todas las rutas)
 app.use((err, req, res, next) => {
   console.error("Error:", err.message);
-  res.status(500).json({ error: "Ha ocurrido un error interno en el servidor." });
+  res
+    .status(500)
+    .json({ error: "Ha ocurrido un error interno en el servidor." });
 });
 
 // Levantar servidor
